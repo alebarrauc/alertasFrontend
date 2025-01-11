527bcc22d819b21a5666ee3825d3a0f22ef0ee27
Alexis <ale.barra@duocuc.cl>
2025-01-11 09:13:54 -0300

    Implement navbar with routes and enter-patient component

---
+++ src/app/component/enter-patient/enter-patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-patient',
+  imports: [],
+  templateUrl: './enter-patient.component.html',
+  styleUrl: './enter-patient.component.css',
+  standalone: true,
+})
+export class EnterPatientComponent {
+
+}
