5041cb8b41cd15d3c8cc490fd0328bfbfdc8016a
Alexis <ale.barra@duocuc.cl>
2025-01-12 08:40:26 -0300

    css for nav / login added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { PatientListComponent } from './app/component/patient-list/patient-list.component';
 import { EnterPatientComponent } from './app/component/enter-patient/enter-patient.component';
+import { UserLoginComponent } from './app/component/user-login/user-login.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -13,6 +14,7 @@ bootstrapApplication(AppComponent, {
       { path: '', redirectTo: '/patient-list', pathMatch: 'full' }, // Redirigir la raíz a 'patient-list'
       { path: 'patient-list', component: PatientListComponent }, // Ruta para el listado de pacientes
       { path: 'enter-patient', component: EnterPatientComponent }, // Ruta para ingresar pacientes
+      { path: 'login-user', component: UserLoginComponent }, // Ruta para Login
     ]),
   ],
 }).catch((err) => console.error(err));
